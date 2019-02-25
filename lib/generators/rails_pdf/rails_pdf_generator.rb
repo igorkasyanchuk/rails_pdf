1be4440a75145fbb877f9257d13f8d6ae06aac41
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2019-02-25 02:00:30 -0800

    fix

--- lib/generators/rails_pdf/rails_pdf_generator.rb
+++ lib/generators/rails_pdf/rails_pdf_generator.rb
@@ -1,28 +1,33 @@
 class RailsPdfGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
+  TYPES = ["basic_invoice", "chart1"]
+
   def create_helper_file
     report = args[0]
 
     if report.nil?
       puts "Please specify report template which you want to generate."
-      puts "rails g rails_pdf invoice <name of report>"
+      puts "Available types: #{TYPES.join(', ')}"
+      puts "rails g rails_pdf <type> <name of report>"
+      puts "sample: rails g rails_pdf #{TYPES[0]} report"
       exit
     end
 
-    case file_name
-    when 'basic_invoice'
-      directory "#{file_name}", "app/pdf/#{report}"
-      gsub_file "app/pdf/#{report}/invoice.pug.erb", '<%= report %>', report
-    when 'chart1'
-      directory "#{file_name}", "app/pdf/#{report}"
-      gsub_file "app/pdf/#{report}/chart.pug.erb", '<%= report %>', report
-    else
-      puts "Template not found. You can use any of: basic_invoice, chart1, "
+    if !TYPES.include?(file_name)
+      puts "Template not found. You can use any of: #{TYPES.join(', ')} "
+      puts "sample: rails g rails_pdf #{TYPES[0]} report"
+      return
     end
 
+    directory "#{file_name}", "app/pdf/#{report}"
 
-
+    case file_name
+    when 'basic_invoice'
+      gsub_file "app/pdf/#{report}/invoice.pug.erb", '<%= report %>', report
+    when 'chart1'
+      gsub_file "app/pdf/#{report}/chart.pug.erb", '<%= report %>', report
+    end
 
     directory "mixins", "app/pdf/mixins"
     directory "shared", "app/pdf/shared"
